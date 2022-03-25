6643f8629a1a25cf02ab3542831d324fe5ee2add
chi.w.chen <chi.w.chen@cgi.com>
2022-03-25 14:55:45 -0400

    Login page CSS with form value access

--- AngularFrontend/src/app/login/login.component.ts
+++ AngularFrontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../services/authentication.service';
+import { RoutingService } from '../services/routing.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form = new FormGroup({
+    inputEmail: new FormControl('', Validators.required),
+    inputPassword: new FormControl('', Validators.required)
+
+  })
+
+  constructor(private routerService:RoutingService, private authService: AuthenticationService) {
+   }
 
   ngOnInit(): void {
   }
+
+  verifyLogin() {
+    let uesrData = {
+      "email": this.form.get('inputEmail')?.value,
+      "password":this.form.get('inputPassword')?.value
+    }
+    console.log(this.form.value);
+    console.log(this.form.get('inputEmail')?.value);
+    console.log(this.form.get('inputPassword')?.value);
+
+  }
+
+
 
 }
