7d049a35177137eea87b074fceecc7956fc49048
thomas.skiffington <thomas.skiffington@cgi.com>
2022-03-28 14:18:56 -0600

    added cors config to communicate with angular application

--- parent/account-service/src/main/java/com/cgi/accountservice/AccountServiceApplication.java
+++ parent/account-service/src/main/java/com/cgi/accountservice/AccountServiceApplication.java
@@ -14,35 +14,12 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class AccountServiceApplication {
 
-    @Value("${spring.rabbitmq.host}")
-    String host;
-    @Value("${spring.rabbitmq.username}")
-    String username;
-    @Value("${spring.rabbitmq.password}")
-    String password;
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
     }
 
-    @Bean
-    CachingConnectionFactory connectionFactory() {
-        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host);
-        cachingConnectionFactory.setUsername(username);
-        cachingConnectionFactory.setPassword(password);
-        return cachingConnectionFactory;
-    }
 
-    @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
 
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
-        return rabbitTemplate;
-    }
 
 }
