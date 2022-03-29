9fe8264bcfe15bf96ba6a28efa5dda94bee32dc2
chi.w.chen <chi.w.chen@cgi.com>
2022-03-29 14:56:32 -0400

    Added image uploads, validation requirements, modal to confirm posting cancellation, and warning msgs

--- AngularFrontend/src/app/sell/sell.component.ts
+++ AngularFrontend/src/app/sell/sell.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication.service';
 import { CategorylistService } from '../services/categorylist.service';
 import { RoutingService } from '../services/routing.service';
@@ -9,7 +10,13 @@ import { RoutingService } from '../services/routing.service';
   styleUrls: ['./sell.component.css'],
 })
 export class SellComponent implements OnInit {
-  categories: any;
+  pForm:any;
+  categories: any = [];
+  urls = new Array<string>();
+  warningMessage: string = '';
+  missingFieldsMessage: string = '';
+  count = 1;
+  maxImageCount = 4;
 
   constructor(
     private catService: CategorylistService,
@@ -25,6 +32,69 @@ export class SellComponent implements OnInit {
       (err: any) => {
         console.log(err);
       }
-    );
+    )
+    this.pForm = new FormGroup({
+      inputTitle: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+      inputCategory: new FormControl('', Validators.required),
+      inputBrand: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      inputCondition: new FormControl('', Validators.required),
+      inputPrice: new FormControl('', [Validators.required, Validators.pattern("^[0-9]+(\.[0-9][0-9]?)?")]),
+      inputDescription: new FormControl('', Validators.required),
+      inputImage: new FormControl('')
+    })
+  };
+
+  preview(event: any) {
+    let files = event.target.files;
+    if (files) {
+      for (let file of files) {
+        let reader = new FileReader();
+
+        reader.onload = (event: any) => {
+          this.urls.push(event.target.result);
+          this.count += 1;
+          
+        };
+        reader.readAsDataURL(file);
+      }
+    }
   }
+
+  verifyProduct(pForm:any){
+    let productData = {
+      "pTitle":this.pForm.get('inputTitle').value,
+      "pBrand":this.pForm.get('inputBrand').value,
+      "pCategory":this.pForm.get('inputBrand').value,
+      "pCondition":this.pForm.get('inputCondition').value,
+      "pPrice":this.pForm.get('inputPrice').value,
+      "pDescription":this.pForm.get('inputDescription').value,
+      "pImage":this.urls
+    }
+    console.log(this.pForm.valid);
+    console.log(this.pForm.get('inputCondition').value);
+    console.log(this.pForm.get('inputTitle').value);
+    console.log(this.pForm.get('inputBrand').value);
+    console.log(this.pForm.get('inputCategory').value);
+    console.log(this.pForm.get('inputPrice').value);
+    console.log(this.pForm.get('inputDescription').value);
+    console.log(this.urls);
+
+    if(this.pForm.valid){
+      // Redirect to see product page or home page?
+    } else {
+      this.missingFieldsMessage = "Please fill in all the fields.";
+    }
+
+
+  }
+
+
+  cancelPost(){
+    document.getElementById("modal-btn")?.click();
+  }
+  
+  returnToHome(){
+    this.routerService.openHome();
+  }
+
 }
