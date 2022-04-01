764229b1fb0453a6ab2aa5cb148964cc20f4de2a
rebecca <rebecca@cgi.com>
2022-04-01 11:09:09 -0400

    Product Database

--- AngularFrontend/src/app/services/productlist.service.ts
+++ AngularFrontend/src/app/services/productlist.service.ts
@@ -13,11 +13,11 @@ export class ProductlistService {
   constructor(private http: HttpClient) { }
 
   getProducts() : Observable<Product[]>{
-    return this.http.get<Product[]>("http://localhost:8076/productMicro/products");
+    return this.http.get<Product[]>("http://localhost:8078/productMicro/products");
   }
 
   getProduct(prodId : string): Observable<Product>{
-    return this.http.get<Product>("http://localhost:8076/productMicro/products/"+prodId);
+    return this.http.get<Product>("http://localhost:8078/productMicro/products/"+prodId);
   }
 }
 
